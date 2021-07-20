9faa58b2bc19345a778e704aedd9163aa2f307ae
Jaspreet Singh <jaspreet.codrity@gmail.com>
2021-07-20 21:08:01 +0530

    to be published on amazon appstore

---
+++ lib/core/viewmodel/view_history_tasks_page_viewModel.dart
@@ -0,0 +1,12 @@
+import 'package:stacked/stacked.dart';
+import 'package:timely/core/services/services.dart';
+
+class ViewHistoryTasksPageViewModel extends StreamViewModel {
+  Services services = Services();
+  Stream getHistoryTasks() {
+    return services.getHistorys();
+  }
+
+  @override
+  Stream get stream => getHistoryTasks();
+}
